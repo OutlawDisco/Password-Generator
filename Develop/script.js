a1f8e1b4e400f33e912dfcc0794b4beb76547c61
OutlawDisco <aoutlaw00@gmail.com>
2023-05-22 18:48:29 -0600

    experimenting with checkbox in html while troubleshooting makeApassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,37 +1,42 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-var length = Number(
-  prompt("Enter the length of your password between 8 -128 characters.")
-);
-var charType = prompt(
-  "Enter what you would like to include in your password: uppercase, lowercase, numbers, and special (for special characters)."
-);
+// function makeAPassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
+var special = "!@#$%^&*";
+var length = document.getElementById("length").value;
 
-var passwordText = document.querySelector("#password");
+function makeAPassword() {
+  var passwordGen = "";
 
-function password() {
-  var charGen = {
-    lowercase: "abcdefghijklmnopqrstuvwxyz",
-    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    numbers: "1234567890",
-    special: "!@#$%^&*",
-  };
-  var charSet = charGen[charType.toLowerCase()];
-  for (i = 0; i < length; i++) {
-    generate += password.charSet(Math.floor(Math.random() * charSet.length));
-  }
-  return generate;
+  passwordGen += lowercase[Math.random() * lowercase.length];
+  console.log(passwordGen);
+
+  passwordGen += uppercase[Math.random() * uppercase.length];
+  console.log(passwordGen);
+
+  passwordGen += numbers[Math.random() * numbers.length];
+  console.log(passwordGen);
+
+  passwordGen += special[Math.random() * special.length];
+  console.log(passwordGen);
+
+  document.getElementById("password").value = passwordGen;
 }
 
-passwordText.value = password();
+// var passwordText = document.querySelector("#password");
+
+// passwordText.value = password();
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", makeAPassword());
 
 function copyPassword() {
   document.getElementById("password").select();
   navigator.clipboard.writeText(copyText.value);
-  alert("Password copied to clipboard!" + copyText.value);
+  // alert("Password copied to clipboard!" + copyText.value);
 }
