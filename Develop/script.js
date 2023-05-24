d5c20b4b08f650565de6c3f5d7e0c4e969f102c0
OutlawDisco <aoutlaw00@gmail.com>
2023-05-23 18:53:41 -0600

    changed div to form in order to validate criteria, fixed if statements to respond to user input

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,6 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
+var generateBtn = document.getElementById("generate");
 
 // function makeAPassword() {
 //   var password = generatePassword();
@@ -8,22 +9,51 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "1234567890";
 var special = "!@#$%^&*";
-var length = document.getElementById("length").value;
+
+// uppercase.appendChild(uppercase);
 
 function makeAPassword() {
   var passwordGen = "";
+  var everyThing = "";
+  var length = parseInt(document.getElementById("length").value);
 
-  passwordGen += lowercase[Math.random() * lowercase.length];
-  console.log(passwordGen);
+  if (
+    !document.getElementById("lowercase").checked &&
+    !document.getElementById("uppercase").checked &&
+    !document.getElementById("numbers").checked &&
+    !document.getElementById("special").checked
+  ) {
+    document.getElementById("password").value =
+      "please check at least one type of character";
+    return;
+  }
 
-  passwordGen += uppercase[Math.random() * uppercase.length];
-  console.log(passwordGen);
-
-  passwordGen += numbers[Math.random() * numbers.length];
-  console.log(passwordGen);
-
-  passwordGen += special[Math.random() * special.length];
-  console.log(passwordGen);
+  if (document.getElementById("lowercase").checked) {
+    passwordGen += lowercase[Math.floor(Math.random() * lowercase.length)];
+    everyThing += lowercase;
+  }
+  if (document.getElementById("uppercase").checked) {
+    passwordGen += uppercase[Math.floor(Math.random() * uppercase.length)];
+    everyThing += uppercase;
+  }
+  if (document.getElementById("numbers").checked) {
+    passwordGen += numbers[Math.floor(Math.random() * numbers.length)];
+    everyThing += numbers;
+  }
+  if (document.getElementById("special").checked) {
+    passwordGen += special[Math.floor(Math.random() * special.length)];
+    everyThing += special;
+  }
+  // for (var i = 0; passwordGen.length < length ; i++) {
+  //   passwordGen += everyThing[Math.floor(Math.random() * everyThing.length)];
+  //   console.log(everyThing);
+  //   console.log(passwordGen);
+  // }
+  while (length > passwordGen.length) {
+    passwordGen += everyThing[Math.floor(Math.random() * everyThing.length)];
+    console.log(everyThing);
+    console.log(passwordGen);
+  }
 
   document.getElementById("password").value = passwordGen;
 }
@@ -33,10 +63,10 @@ function makeAPassword() {
 // passwordText.value = password();
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", makeAPassword());
+generateBtn.addEventListener("click", makeAPassword);
 
-function copyPassword() {
-  document.getElementById("password").select();
-  navigator.clipboard.writeText(copyText.value);
-  // alert("Password copied to clipboard!" + copyText.value);
-}
+// // function copyPassword() {
+//document.getElementById("password").select();
+//   navigator.clipboard.writeText(copyText.value);
+//   // alert("Password copied to clipboard!" + copyText.value);
+// }
